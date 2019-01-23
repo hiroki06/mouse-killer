f9d376d6a86527c986456a7de70872ec9062215c
Masanori Hiroki <hiroki.masanori06@gmail.com>
2019-01-23 21:59:55 +0900

    eventを付与する関数を作成

--- js/content.js
+++ js/content.js
@@ -1,30 +1,45 @@
 'use strict';
-(function(){
-	// var で宣言したものは、関数内で使うという便宜的myルール。
-	// TODO: 若干safetyじゃない気もするので、後ほど改良
-	// ちなみに、この程度の開発レベルなので コメントで次やることを管理しているのである。
-	var elmName = "#rcnt .r a";
-	let forwardKey = 78,
-		backwardKey = 80,
-		currentLinkNum = -1,
-		lastLinkNum = $(elmName).length - 1;
+(function () {
+  class Clicklink {
+    constructor(setting) {
+      let elmName = setting.elmName,
+        forwardKey = parseFloat(setting.forwardKey),
+        backwardKey = parseFloat(setting.backwardKey),
+        currentLinkNum = -1,
+        lastLinkNum = $(elmName).length - 1;
 
-	const focusElm = (elmNum) => {
-		$(elmName).eq(elmNum).focus();
-	};
-
-	$(document).on('keydown', function(e) {
-		if (e.ctrlKey && e.shiftKey) {
-			switch(e.keyCode){
-				case forwardKey:
-					currentLinkNum += 1;
-					currentLinkNum <= lastLinkNum ? focusElm(currentLinkNum) : focusElm(currentLinkNum = 0);
-					break;
-				case backwardKey:
-					currentLinkNum -= 1;
-					currentLinkNum >= 0 ? focusElm(currentLinkNum) :focusElm(currentLinkNum = lastLinkNum);
-					break;
-			}
-		}
-	});
+      $(document).on('keydown', function (e) {
+        if (e.ctrlKey && e.shiftKey) {
+          switch (e.keyCode) {
+            case forwardKey:
+              currentLinkNum += 1;
+              currentLinkNum <= lastLinkNum ? focusElm(elmName,currentLinkNum) : focusElm(elmName,currentLinkNum = 0);
+              break;
+            case backwardKey:
+              currentLinkNum -= 1;
+              currentLinkNum >= 0 ? focusElm(elmName,currentLinkNum) : focusElm(elmName,currentLinkNum = lastLinkNum);
+              break;
+          }
+        }
+      });
+    };
+  };
+  const focusElm = (elmName,elmNum) => {
+    $(elmName).eq(elmNum).focus();
+  };
+  const addLinkFocusEvent = () => {
+    chrome.runtime.sendMessage({ method: "getLocalStorage"}, function (response) {
+      let settings = JSON.parse(response.setting);
+      let this_url = location.href;
+      settings.forEach(keyBinds => {
+        let reg = new RegExp(keyBinds.url);
+        if( reg.test(this_url) ) {
+          keyBinds.keysets.forEach(keyBind => {
+            new Clicklink(keyBind);
+          })
+        }
+      });
+    });
+  };
+  addLinkFocusEvent();
 }());
