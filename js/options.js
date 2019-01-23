fe4d08009d7d468b855a94b4ac8f5ca04a44794b
Masanori Hiroki <hiroki.masanori06@gmail.com>
2019-01-23 22:10:19 +0900

    option.jsのインデントを修正

--- js/options.js
+++ js/options.js
@@ -1,75 +1,74 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    let vm = new Vue({
-        el:"#options",
-        data: {
-            urls: [
-                {
-                    url: "google",
-                    keysets: [
-                        {
-                            // id: 1,
-                            // forwardKey: 80,
-                            // backwardKey: 12,
-                            // elmName: "aaa"
-                        }                    ]
-                }
-            ]
+  let vm = new Vue({
+    el:"#options",
+    data: {
+      urls: [
+        {
+          url: "google",
+          keysets: [
+            {
+              // id: 1,
+              // forwardKey: 80,
+              // backwardKey: 12,
+              // elmName: "aaa"
+            }                    ]
+        }
+      ]
+    },
+    watch: {
+      urls: {
+        handler: function() {
+          localStorage.setItem('setting', JSON.stringify(this.urls));
+          // ls.set('clicklinData', this.urls);
         },
-        watch: {
-            urls: {
-                handler: function() {
-                    localStorage.setItem('setting', JSON.stringify(this.urls));
-                    // ls.set('clicklinData', this.urls);
-                },
-                    deep: true
-            }
-        },
-        mounted: function() {
-          this.urls = JSON.parse(localStorage.getItem('setting')) || [];
-        },
-        methods: {
-            addSetting:function () {
-                // this.urls.push(this.newurl);
-            },
-            deleteUrl: function (index) {
-                if (confirm('are you sure?')) {
-                  this.urls.splice(index, 1);
-                }
-            },
-            deleteItem: function(urlIndex,itemIndex) {
-                if (confirm('are you sure?')) {
-                  this.urls[urlIndex].keysets.splice(itemIndex, 1);
-                }
-            },
-            addUrl: function () {
-                // let len = this.urls.length + 1;
-                this.urls.push({
-                    url:"",
-                    keysets:[{
-                        id:0,
-                        forwardKey:"",
-                        backwardKey:"",
-                        elmName:""
-                    }]
-                });
-            },
-            addItem: function (urlIndex,itemIndex) {
-                // let len = this.urls.length + 1;
-                this.urls[urlIndex].keysets.push({
-                    id:itemIndex+1,
-                    forwardKey:"",
-                    backwardKey:"",
-                    elmName:""
-                });
-            },
-            saveItems: function() {
-                localStorage.setItem('setting', JSON.stringify(this.urls));
-            },
-            showDataToConsole: function () {
-                console.dir(this.urls);
-            }
-        },
-    });
+          deep: true
+      }
+    },
+    mounted: function() {
+      this.urls = JSON.parse(localStorage.getItem('setting')) || [];
+    },
+    methods: {
+      addSetting:function () {
+        // this.urls.push(this.newurl);
+      },
+      deleteUrl: function (index) {
+        if (confirm('are you sure?')) {
+          this.urls.splice(index, 1);
+        }
+      },
+      deleteItem: function(urlIndex,itemIndex) {
+        if (confirm('are you sure?')) {
+          this.urls[urlIndex].keysets.splice(itemIndex, 1);
+        }
+      },
+      addUrl: function () {
+        // let len = this.urls.length + 1;
+        this.urls.push({
+          url:"",
+          keysets:[{
+            id:0,
+            forwardKey:"",
+            backwardKey:"",
+            elmName:""
+          }]
+        });
+      },
+      addItem: function (urlIndex,itemIndex) {
+        this.urls[urlIndex].keysets.push({
+          id:itemIndex+1,
+          forwardKey:"",
+          backwardKey:"",
+          elmName:""
+        });
+      },
+      saveItems: function() {
+        localStorage.setItem('setting', JSON.stringify(this.urls));
+      },
+      showDataToConsole: function () {
+        console.dir(this.urls);
+      }
+    },
+  });
 }());
